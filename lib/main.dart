b06ee34c8e18c552b1a7621bf3d3b6e2d4bc3c55
krupa parmar <149374671+Krupaparmar30@users.noreply.github.com>
2024-04-11 23:58:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:counter_app/utils/homePage/icons.dart';
 import 'package:counter_app/utils/homePage/DymanicList.dart';
+import 'package:counter_app/utils/homePage/iconsediter.dart';
+import 'package:counter_app/utils/homePage/krupa.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:counter_app/utils/homePage/chessBorad.dart';
 import 'package:counter_app/utils/homePage/mapApp/mapApp.dart';
 void main()
 {
-  runApp(iconsApp());
+  runApp(iconsEddApp());
 }
 
-class iconsApp extends StatelessWidget {
-  const iconsApp({super.key});
+class iconsEddApp extends StatelessWidget {
+  const iconsEddApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,5 +22,3 @@ class iconsApp extends StatelessWidget {
     );
   }
 }
-
-
