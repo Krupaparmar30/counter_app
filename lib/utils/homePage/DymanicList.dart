b2faea79941f133be1960f349437c92928974019
krupa parmar <149374671+Krupaparmar30@users.noreply.github.com>
2024-04-11 23:57:46 +0530

    Update DymanicList.dart

--- lib/utils/homePage/DymanicList.dart
+++ lib/utils/homePage/DymanicList.dart
@@ -26,72 +26,70 @@ class _homeScreenState extends State<homeScreen> {
       ),
       body: SingleChildScrollView(
         child: Align(
-          
           child: Column(
-            children: [
-              box(),
-              box(),
-              box(),
-              box(),
-              box(),
-              box(),
-              box(),
-        
-        
-        
-        
-        
-            ],
+            children: List.generate(box1.length, (index) =>box(index: box1[index]) ),
           ),
         ),
       ),
-        floatingActionButton: Row(
-
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(onPressed: (){}
-            ,child: Icon(Icons.add,color: Colors.blue.shade500,),),
-
-         SizedBox(width: 20,),
-
-
-    FloatingActionButton(onPressed: (){}
-    ,child: Icon(Icons.remove,color: Colors.blue.shade500,),),
-
-    ],
-        ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                box1.add(box1.length + 1);
+              });
+            },
+            child: Icon(
+              Icons.add,
+              color: Colors.blue.shade500,
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                box1.removeAt(box1.length-1);
+              });
+            },
+            child: Icon(
+              Icons.remove,
+              color: Colors.blue.shade500,
+            ),
+          ),
+        ],
+      ),
     );
   }
-  List box1=[
-    0
-  ];
 
-  Padding box() {
+  List<int> box1 = [0];
+
+  Padding box({required index}) {
     return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              width: double.infinity,
-
-              decoration: BoxDecoration(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
-                color: Colors.pink,
-              ),
-              child: Center(child: Text('1',style: TextStyle(color: Colors.white,fontSize: 32,fontWeight: FontWeight.bold),)),
-
-
-
-            )
-            ,
-
-          ],
-
-        ),
-
-
-      );
-
+                color: (index % 2 == 0)
+                    ? Colors.pink.shade300
+                    : Colors.pink.shade400),
+            child: Center(
+                child: Text(
+              '$index',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold),
+            )),
+          ),
+        ],
+      ),
+    );
   }
 }
